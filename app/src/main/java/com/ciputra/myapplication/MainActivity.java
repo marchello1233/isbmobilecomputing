23e67131a68bc369bb5142401a9ea044bd3090f3
marchellohans <msumargo@student.ciputra.ac.id>
2020-10-26 10:30:49 +0700

    first commit

---
+++ app/src/main/java/com/ciputra/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ciputra.myapplication;
+
+import dataBinding
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+}
